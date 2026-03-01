5951ad5d26c895c0ff42d44af9ec30a833aeeced
unknown <mohamedhisham1511@gmail.com>
2026-03-01 15:01:38 +0200

    feat: add about page route and lock dependencies

---
+++ NextJs Essentials/app/about/page.js
@@ -0,0 +1,9 @@
+export default function About() {
+  return (
+    <main>
+      <h1>About This Course</h1>
+      <p>This course is designed to teach you the essentials of Next.js, a powerful React framework for building server-rendered applications.</p>
+      <p>By the end of this course, you will have a solid understanding of how to create dynamic web applications using Next.js.</p>
+    </main>
+  );
+}
