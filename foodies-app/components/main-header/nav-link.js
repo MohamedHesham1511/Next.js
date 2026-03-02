66e816c2694c398ea2ff677be9c04a884b9280f5
unknown <mohamedhisham1511@gmail.com>
2026-03-03 00:09:25 +0200

    feat: implement meals page with grid layout and meal items display

--- foodies-app/components/main-header/nav-link.js
+++ foodies-app/components/main-header/nav-link.js
@@ -6,7 +6,7 @@ import { usePathname } from "next/navigation";
 export const NavLink = ({ href, children }) => {
   const currentPath = usePathname();
   return (
-    <Link href={href} className={currentPath === href ? `${classes.link} ${classes.active}` : classes.link}>
+    <Link href={href} className={currentPath.startsWith(href) ? `${classes.link} ${classes.active}` : classes.link}>
       {children}
     </Link>
   );
