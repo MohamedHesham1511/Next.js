7b6cfd9c4a105aa74fe39006e07301b722580a0e
unknown <mohamedhisham1511@gmail.com>
2026-03-03 23:16:20 +0200

    feat: add meal sharing form with image preview and server action

--- foodies-app/components/meals/image-picker.js
+++ foodies-app/components/meals/image-picker.js
@@ -1,19 +1,49 @@
 "use client";
 import classes from "./image-picker.module.css";
-import { useRef } from "react";
+import { useRef, useState } from "react";
+import Image from "next/image";
 
 export const ImagePicker = ({ label, name }) => {
+  const [pickedImage, setPickedImage] = useState();
   const imageInput = useRef();
 
   const handlePickImage = () => {
     imageInput.current.click();
   };
 
+  const handleImageChange = (event) => {
+    const pickedFile = event.target.files[0];
+
+    if (!pickedFile) {
+      setPickedImage(null);
+      return;
+    }
+
+    const fileReader = new FileReader();
+    fileReader.onload = () => {
+      setPickedImage(fileReader.result);
+    };
+    fileReader.readAsDataURL(pickedFile);
+  };
+
   return (
     <div className={classes.picker}>
       <label htmlFor={name}>{label}</label>
       <div className={classes.controls}>
-        <input ref={imageInput} type='file' id={name} name={name} accept='image/png, image/jpeg' className={classes.input} />
+        <div className={classes.preview}>
+          {!pickedImage && <p>Please pick an image.</p>}
+          {pickedImage && <Image src={pickedImage} alt='Preview' fill />}
+        </div>
+        <input
+          ref={imageInput}
+          type='file'
+          onChange={handleImageChange}
+          id={name}
+          name={name}
+          accept='image/png, image/jpeg'
+          className={classes.input}
+          required
+        />
         <button type='button' onClick={handlePickImage} className={classes.button}>
           Pick an Image
         </button>
