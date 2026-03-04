8a2e32bf2a07df6ebe60af3fa0b6a98ea399dea2
unknown <mohamedhisham1511@gmail.com>
2026-03-04 13:04:19 +0200

    feat: add meal sharing form with validation and error handling

--- foodies-app/lib/actions.js
+++ foodies-app/lib/actions.js
@@ -1,8 +1,13 @@
 "use server";
 import { redirect } from "next/navigation";
 import { saveMeal } from "./meals";
+import { revalidatePath } from "next/cache";
 
-export const shareMeal = async (formData) => {
+const isInvalidText = (text) => {
+  return !text || text.trim() === "";
+};
+
+export const shareMeal = async (prevState, formData) => {
   const meal = {
     title: formData.get("title"),
     summary: formData.get("summary"),
@@ -11,6 +16,23 @@ export const shareMeal = async (formData) => {
     creator: formData.get("name"),
     creator_email: formData.get("email"),
   };
+
+  if (
+    isInvalidText(meal.title) ||
+    isInvalidText(meal.summary) ||
+    isInvalidText(meal.instructions) ||
+    isInvalidText(meal.creator) ||
+    isInvalidText(meal.creator_email) ||
+    !meal.creator_email.includes("@") ||
+    !meal.image ||
+    meal.image.size === 0
+  ) {
+    return {
+      message: "Invalid input. Please fill in all fields correctly.",
+    };
+  }
+
   await saveMeal(meal);
+  revalidatePath("/meals"); // Revalidate the meals page to show the new meal
   redirect("/meals"); // Redirect to the meals page after saving the meal
 };
